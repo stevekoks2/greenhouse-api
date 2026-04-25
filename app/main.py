c2db4e41471f42c4e9d06efdda5836f3718b3f34
Steve Koks <stevekoksg@gmail.com>
2026-04-25 20:26:06 +0300

    sensors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import asyncio
 from threading import Lock
 
 import paho.mqtt.client as mqtt
@@ -9,12 +8,20 @@ app = FastAPI(title="GreenHouse API")
 
 MQTT_BROKER = "192.168.1.37"
 MQTT_PORT = 1488
-RELAY_TOPIC = "home/relay"
-HUMIDITY_TOPIC = "home/sensor/humidity"
+
+DEVICE_RELAY_TOPIC = "/device/relay/"
+DEVICE_AIR_TOPIC = "/device/air/"
+DEVICE_SHINE_TOPIC = "/device/shine/"
+
+SENSOR_HUMIDITY_TOPIC = "/sensor/humidity/"
+SENSOR_TEMPERATURE_TOPIC = "/sensor/temperature/"
+SENSOR_SHINE_TOPIC = "/sensor/shine/"
+SENSOR_WATER_LEVEL_TOPIC = "/sensor/water_lvl"
+STATS = "/statistic/"
+
 
 origins = ["*"]
 
-# Хранилище для последних значений датчиков
 sensor_data = {"humidity": None, "temperature": None, "last_update": None}
 
 data_lock = Lock()
@@ -27,10 +34,15 @@ def on_message(client, userdata, msg):
 
     print(f"Received `{payload}` from `{topic}` topic")
 
-    # Сохраняем данные в зависимости от топика
     with data_lock:
-        if topic == HUMIDITY_TOPIC:
+        if topic == SENSOR_HUMIDITY_TOPIC:
             sensor_data["humidity"] = payload
+        elif topic == SENSOR_TEMPERATURE_TOPIC:
+            sensor_data["temperature"] = payload
+        elif topic == SENSOR_SHINE_TOPIC:
+            sensor_data["shine"] = payload
+        elif topic == SENSOR_WATER_LEVEL_TOPIC:
+            sensor_data["water_lvl"] = payload
 
 
 app.add_middleware(
@@ -48,9 +60,10 @@ mqtt_client.on_message = on_message
 @app.on_event("startup")
 async def startup_event():
     mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
-    # Подписываемся на все нужные топики при старте
-    mqtt_client.subscribe(HUMIDITY_TOPIC)
-    mqtt_client.subscribe("home/sensor/#")  # можно использовать wildcard
+    mqtt_client.subscribe(SENSOR_HUMIDITY_TOPIC)
+    mqtt_client.subscribe(SENSOR_TEMPERATURE_TOPIC)
+    mqtt_client.subscribe(SENSOR_SHINE_TOPIC)
+    mqtt_client.subscribe(SENSOR_WATER_LEVEL_TOPIC)
     mqtt_client.loop_start()
 
 
@@ -70,7 +83,7 @@ def relay(state):
     if state not in ["0", "1"]:
         raise HTTPException(status_code=400, detail="State must be 0 or 1")
 
-    result = mqtt_client.publish(RELAY_TOPIC, state)
+    result = mqtt_client.publish(DEVICE_RELAY_TOPIC, state)
 
     if result.rc == mqtt.MQTT_ERR_SUCCESS:
         return {"status": "success", "sent_state": state}
@@ -78,7 +91,33 @@ def relay(state):
         raise HTTPException(status_code=500, detail="Failed to send MQTT message")
 
 
-@app.get("/sensors/humidity/")
+@app.post("/devices/air/{state}")
+def air(state):
+    if state not in ["0", "1"]:
+        raise HTTPException(status_code=400, detail="State must be 0 or 1")
+
+    result = mqtt_client.publish(DEVICE_AIR_TOPIC, state)
+
+    if result.rc == mqtt.MQTT_ERR_SUCCESS:
+        return {"status": "success", "sent_state": state}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to send MQTT message")
+
+
+@app.post("/devices/shine/{state}")
+def shine(state):
+    if state not in ["0", "1"]:
+        raise HTTPException(status_code=400, detail="State must be 0 or 1")
+
+    result = mqtt_client.publish(DEVICE_SHINE_TOPIC, state)
+
+    if result.rc == mqtt.MQTT_ERR_SUCCESS:
+        return {"status": "success", "sent_state": state}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to send MQTT message")
+
+
+@app.get("/sensor/humidity/")
 def get_humidity():
     with data_lock:
         if sensor_data["humidity"] is None:
@@ -89,7 +128,34 @@ def get_humidity():
         }
 
 
-@app.get("/sensors/all/")
-def get_all_sensors():
+@app.get("/sensor/temperature/")
+def get_temp():
     with data_lock:
-        return sensor_data
+        if sensor_data["temperature"] is None:
+            raise HTTPException(status_code=404, detail="No data received yet")
+        return {
+            "humidity": sensor_data["humidity"],
+            "last_update": sensor_data["last_update"],
+        }
+
+
+@app.get("/sensor/shine/")
+def get_shine():
+    with data_lock:
+        if sensor_data["shine"] is None:
+            raise HTTPException(status_code=404, detail="No data received yet")
+        return {
+            "humidity": sensor_data["humidity"],
+            "last_update": sensor_data["last_update"],
+        }
+
+
+@app.get("/sensor/water-lvl/")
+def get_water_lvl():
+    with data_lock:
+        if sensor_data["water_lvl"] is None:
+            raise HTTPException(status_code=404, detail="No data received yet")
+        return {
+            "humidity": sensor_data["humidity"],
+            "last_update": sensor_data["last_update"],
+        }
